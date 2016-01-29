6fd5cfbdfe8f3757d76e7eb2f16e76f6f5e68cd0
Isaac Reid-Guest <isaac.reid.guest@gmail.com>
2016-01-29 11:33:13 +0000

    Update rawcanvas-test-1.js

--- rawcanvas-test-1.js
+++ rawcanvas-test-1.js
@@ -8,6 +8,7 @@ var drawGrid = function(grid, gridItems){
   var ctx = canvas.getContext('2d');
   var xLen = 1600/Number(grid[0].length);
   var yLen = 1200/Number(grid[0].length);
+  /*
   for(var xCount in grid){
     for(var yCount in grid[xCount]){
       
@@ -21,6 +22,29 @@ var drawGrid = function(grid, gridItems){
         ctx.fillStyle = tmpcol2;
       }else{
         var x = "rgb(50,60,0)";
+      }
+      ctx.fillRect (xCount*xLen, yCount*yLen, xLen, yLen);
+    }
+  }*/
+  for(var xCount in grid){
+    for(var yCount in grid[xCount]){
+      
+      var step = Number(grid[xCount][yCount].step);
+      var type = grid[xCount][yCount].type;
+            switch(type){
+        case 1:
+          // Normal walking space
+          break;
+        case 2:
+          console.log("Player");
+          // Player position
+          ctx.fillStyle = "rgb(200,0,0)";
+          break;
+        default:
+          // Walled, blocking space
+          console.log("Wall");
+          ct.fillStyle = "rgb(0,0,200)";
+          //iso.add(Shape.Pyramid(new Point(xCount, yCount, 1)), blue);
       }
       ctx.fillRect (xCount*xLen, yCount*yLen, xLen, yLen);
     }
