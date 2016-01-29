fb14ff4fed83c75004372589c2ec27d6128703e5
Isaac Reid-Guest <isaac.reid.guest@gmail.com>
2016-01-29 11:28:43 +0000

    Update rawcanvas-test-1.js

--- rawcanvas-test-1.js
+++ rawcanvas-test-1.js
@@ -10,7 +10,17 @@ var drawGrid = function(grid, gridItems){
   var yLen = 1200/Number(grid[0].length);
   for(var xCount in grid){
     for(var yCount in grid[xCount]){
-      ctx.fillStyle = "rgb(200,0,0)";
+      
+      var step = Number(grid[xCount][yCount].step);
+      var type = grid[xCount][yCount].type;
+      if(step>0){
+        var tmpcol1 = [50, 60, (3*step)+30];
+        var tmpcol2 = "rgb(" + tmpcol1.join(",") + ")";
+        //"rgb(200,0,0)";
+        ctx.fillStyle = tmpcol2;
+      }else{
+        var x = "rgb(50,60,60)";
+      }
       ctx.fillRect (xCount*xLen, yCount*yLen, xLen, yLen);
     }
   }
