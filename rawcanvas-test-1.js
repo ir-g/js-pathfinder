ebd3c0129714ba25ba6e2058e13dd0d0d19a3bec
Isaac Reid-Guest <isaac.reid.guest@gmail.com>
2016-01-29 11:21:39 +0000

    Create rawcanvas-test-1.js

---
+++ rawcanvas-test-1.js
@@ -0,0 +1,18 @@
+var drawGrid = function(grid, gridItems){
+  var canvas = document.getElementById('canvas');
+  if (!canvas.getContext){
+    // canvas-unsupported code here
+    document.write("CANVAS IS NOT SUPPORTED IN THIS BROWSER. Get Opera, Chrome, Firefox today...");
+    return;
+  }
+  var ctx = canvas.getContext('2d');
+  var xLen = 1600/Number(grid[0].length);
+  var yLen = 1200/Number(grid[0].length);
+  for(var xCount in grid){
+    for(var yCount in grid[xCount]){
+      ctx.fillStyle = "rgb(200,0,0)";
+      ctx.fillRect (10, 10, 55, 50);
+    }
+  }
+}
+findPath(true, drawGrid);
